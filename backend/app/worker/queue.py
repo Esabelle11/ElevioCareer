066944154da851e5a61d65bc869a89c60e3c0e82
CHEN YAN TENG <esabelle@CHENs-MBP.realtek>
2026-05-05 17:35:54 +0800

    worker environment

---
+++ backend/app/worker/queue.py
@@ -0,0 +1,14 @@
+# worker/queue.py
+from rq import Queue
+from redis import Redis
+from app.job_services.run_scrapper import run_scrapper
+import os
+
+# redis_conn = Redis()
+redis_conn = Redis.from_url(os.getenv("REDIS_URL", "redis://localhost:6379"))
+queue = Queue(connection=redis_conn)
+
+def enqueue_scrape_job(location, keyword):
+    job = queue.enqueue(run_scrapper, location, keyword)
+    
+    return job.id
